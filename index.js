a66a38971b59eca2e0490927bb836f4d29a683a7
Josh Feingold <jfeingold@jfeingo-ltmavvx.internal.salesforce.com>
2025-06-19 14:49:16 -0500

    asdfasdfasdf

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 export const SOME_CONSTANT_VALUE = 'SOMECONSTANTVALUE';
 
 export const SOME_OTHER_CONSTANT_VALUE = 'asdfasdfasdf';
+export const SOME_OTHER_CONSTANT_VALUE2 = 'asdfasdfasdf';
+export const SOME_OTHER_CONSTANT_VALUE3 = 'asdfasdfasdf';
 
 export function isTruthy(arg) {
     return !!arg
